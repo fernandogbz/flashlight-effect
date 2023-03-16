e90d09c5066641bc095c56f6cc69be8b1c697e64
Fernando Jimenez <fgjv06@gmail.com>
2023-03-16 18:21:19 -0300

    JS - setting flashlight style property to --X&Ypos in px

--- script.js
+++ script.js
@@ -17,4 +17,7 @@ console.log(isTouchDevice());
 function getMousePosition(e) {
   mouseX = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
   mouseY = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+
+  flashlight.style.setProperty("--Xpos", mouseX + "px");
+  flashlight.style.setProperty("--Ypos", mouseY + "px");
 }
